464b1de5e29af75a6ff2bfa167a5dcf9450b599e
easy957 <easy957@gmail.com>
2022-01-17 15:00:14 +0200

    side-menu fix

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,18 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const siteWrapper = document.querySelector("[data-site-wrapper]")
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const mainContainer = document.querySelector("[data-main-container]");
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
-      siteWrapper.classList.toggle("menu-open");
+      mainContainer.classList.toggle("menu-open");
 
     })
 
     mobileBtnClose.addEventListener('click', () => {
       mobileMenuRef.classList.toggle("is-open");
-      siteWrapper.classList.toggle("menu-open");
+      mainContainer.classList.toggle("menu-open");
 
     });
   })()
