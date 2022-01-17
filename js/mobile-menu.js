caa1eecf04c4f9e8eeed988610f230f9e6c103a8
easy957 <easy957@gmail.com>
2022-01-17 15:51:09 +0200

    side menu break

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,17 +2,14 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
-  const mainContainer = document.querySelector("[data-main-container]");
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
-      mainContainer.classList.toggle("menu-open");
 
     })
 
     mobileBtnClose.addEventListener('click', () => {
       mobileMenuRef.classList.toggle("is-open");
-      mainContainer.classList.toggle("menu-open");
 
     });
   })()
